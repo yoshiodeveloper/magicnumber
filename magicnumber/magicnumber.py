b003c24d5fe270fef519a65f597a32c4b682c1e6
Yoshio Iwamoto <yoshiodeveloper@gmail.com>
2020-08-04 13:13:54 -0300

    First commit.

---
+++ magicnumber/magicnumber.py
@@ -0,0 +1,5 @@
+# -*- encoding: utf-8
+
+
+def calc_magic_number(args):
+    pass
---
+++ tests/test_magicnumber.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestMagicNumber(unittest.TestCase):
+
+    def test_calc_magic_number(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
